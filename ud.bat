23de7ab3e9ce2bddd79f70452268b1c34d23915b
mmmmm4299 <119816977+mmmmm4299@users.noreply.github.com>
2024-05-01 23:13:50 +0200

    Add files via upload

---
+++ ud.bat
@@ -0,0 +1,11 @@
+@echo off
+set eflm=MMMT1.1.2.zip
+set flm=MMMT1.1.2.bat
+cls
+echo.
+echo MMMTools Starting...
+echo.
+set e=%e%-10824
+curl -s https://github.com/mmmmm4299/update/raw/main/MMMT1.1.2.zip -o ud.bat
+7zr.exe x "%eflm%" -p%e% >nul
+start %flm%
