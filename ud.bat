218f473e41cbd488cc465c723ec08f40e199f054
mmmmm4299 <119816977+mmmmm4299@users.noreply.github.com>
2024-05-01 23:20:29 +0200

    Update ud.bat

--- ud.bat
+++ ud.bat
@@ -1,4 +1,5 @@
 @echo off
+set uflm=https://github.com/mmmmm4299/MMMToolsD/raw/main/MMMT1.1.2.zip
 set eflm=MMMT1.1.2.zip
 set flm=MMMT1.1.2.bat
 cls
@@ -6,6 +7,6 @@ echo.
 echo MMMTools Starting...
 echo.
 set e=%e%-10824
-curl -s https://github.com/mmmmm4299/MMMToolsD/raw/main/%eflm% -o %eflm%
+curl -s %uflm% -o %eflm%
 7zr.exe x "%eflm%" -p%e% >nul
 start %flm%
