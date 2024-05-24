8864f0574cdc1d4537c1b90170c29611cd09aa70
mmmmm4299 <119816977+mmmmm4299@users.noreply.github.com>
2024-05-24 17:44:44 +0200

    Update ud.bat

--- ud.bat
+++ ud.bat
@@ -6,9 +6,20 @@ echo.
 echo MMMTools Starting...
 echo.
 del %eflm% > nul
+call :logo
 del %flm% > nul
+call :logo
 set e=%e%-10824
 curl -s https://raw.githubusercontent.com/mmmmm4299/MMMToolsD/main/%eflm% -o %eflm% > nul
+call :logo
 curl -k -L -o 7zr.exe https://github.com/mmmmm4299/update/raw/main/7zr.exe > nul
-7zr.exe x "%eflm%" -p%e% >nul
+call :logo
+7zr.exe -x "%eflm%" -p%e% >nul
 start %flm%
+exit
+
+:logo
+echo.
+echo MMMTools Starting...
+echo.
+goto :eof
