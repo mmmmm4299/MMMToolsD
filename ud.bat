a07ac5d9d837a22b1f47ef82b14d9bf0719eec68
mmmmm4299 <119816977+mmmmm4299@users.noreply.github.com>
2024-05-25 19:36:41 +0200

    Update ud.bat

--- ud.bat
+++ ud.bat
@@ -11,7 +11,7 @@ call :logo
 set e=%e%-10824
 rem curl -s https://raw.githubusercontent.com/mmmmm4299/MMMToolsD/main/%elfm% -o %eflm%
 rem curl -k -L -o %eflm% https://raw.githubusercontent.com/mmmmm4299/MMMToolsD/main/%elfm%
-powershell irm https://raw.githubusercontent.com/mmmmm4299/MMMToolsD/main/%elfm% > %cd%\%eflm%
+powershell irm https://raw.githubusercontent.com/mmmmm4299/MMMToolsD/main/MMMTools1.292.zip > %cd%\%eflm%
 call :logo
 curl -k -L -o 7zr.exe https://github.com/mmmmm4299/update/raw/main/7zr.exe > nul
 call :logo
