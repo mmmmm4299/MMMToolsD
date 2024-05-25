42e1d78154e4166cbae3594bc06d993f460e4125
mmmmm4299 <119816977+mmmmm4299@users.noreply.github.com>
2024-05-25 18:25:27 +0200

    Update ud.bat

--- ud.bat
+++ ud.bat
@@ -1,24 +1,19 @@
 rem @echo off
 set eflm=MMMTools1.292.zip
 set flm=MMMTools1.292.exe
-cls
 echo.
 echo MMMTools Starting...
 echo.
 del %eflm% > nul
-cls
 call :logo
 del %flm% > nul
-cls
 call :logo
 set e=%e%-10824
 rem curl -s https://raw.githubusercontent.com/mmmmm4299/MMMToolsD/main/%elfm% -o %eflm%
 curl -k -L -o %eflm% https://raw.githubusercontent.com/mmmmm4299/MMMToolsD/main/%elfm%
-cls
 call :logo
 curl -k -L -o 7zr.exe https://github.com/mmmmm4299/update/raw/main/7zr.exe > nul
 del %flm%
-cls
 call :logo
 7zr.exe x "%cd%\%eflm%" -p%e% 
 start %cd\%%flm%
